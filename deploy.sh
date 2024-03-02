e9de1d699ad2b4c404b6f18d1eedd0bfe2214060
nyko <nicksiv@disroot.org>
2024-03-02 11:20:04 +0200

    rebuilding site Sat Mar  2 11:20:04 AM EET 2024

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,9 @@
 
 # If a command fails then the deploy stops
 set -e
-#git remote set-url origin git@github.com:nicksiv/nicksiv.github.io.git
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 cd ~/nicksiv.github.io
 # Build the project.
-#hugo -t ezhil # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-#cd public
 
 # Add changes to git.
 git add .
@@ -22,4 +17,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push #origin master
+git push 
