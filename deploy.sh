62ed0ad2763b8215963a228e6c18fc8c7c1b3778
nyko <nicksiv@disroot.org>
2024-03-02 11:15:14 +0200

    rebuilding site Sat Mar  2 11:15:14 AM EET 2024

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # If a command fails then the deploy stops
 set -e
-git remote set-url origin git@github.com:nicksiv/nicksiv.github.io.git
+#git remote set-url origin git@github.com:nicksiv/nicksiv.github.io.git
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
