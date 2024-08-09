71bd6fb9e1fae02b67112edd6bdfc3c13d7674fc
nyk0si <nicksiv@disroot.org>
2024-08-09 17:10:05 +0300

    rebuilding site Fri Aug  9 05:10:05 PM EEST 2024

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # If a command fails then the deploy stops
 set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-cd ~/org/nicksiv.github.io
+cd ~/Documents/nicksiv.github.io
 # Build the project.
 
 # Add changes to git.
