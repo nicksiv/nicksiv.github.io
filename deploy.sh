ce84aa81b59b548d86623b2d3796fc584014dd76
nyk0si <nicksiv@disroot.org>
2024-08-12 12:49:25 +0300

    rebuilding site Mon Aug 12 12:49:25 PM EEST 2024

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/nicksiv.github.io
-python3 generate.py
+python3 generate.py ;
 # If a command fails then the deploy stops
 set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
