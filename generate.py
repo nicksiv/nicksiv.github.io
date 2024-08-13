4fd737feaed944500d598181237d498c230be02f
nyk0si <nicksiv@disroot.org>
2024-08-13 19:32:34 +0300

    rebuilding site Tue Aug 13 07:32:34 PM EEST 2024

--- generate.py
+++ generate.py
@@ -2,16 +2,16 @@
 # Aug 2024
 
 import os
-os.system("")
-import datetime,time
-import sys
-import subprocess
-import webbrowser
-import glob
-import shutil
-from random import random, randint 
-from datetime import date
-import re
+#os.system("")
+#import datetime,time
+#import sys
+#import subprocess
+#import webbrowser
+#import glob
+#import shutil
+#from random import random, randint 
+#from datetime import date
+#import re
 username=os.getlogin()
 srcFolder="/home/"+username+"/nicksiv.github.io/src/"
 destFolder="/home/"+username+"/nicksiv.github.io/"
@@ -19,13 +19,13 @@ destFolder="/home/"+username+"/nicksiv.github.io/"
 list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(srcFolder, x)), os.listdir(srcFolder) ),reverse=False )
 head=os.path.join(srcFolder,"header.html")
 foot=os.path.join(srcFolder,"footer.html")
-pages="<h2>Page List (site index)</h2><ul>"
+pages="<h1>Page List (site index)</h1><ul>"
 
 for filename in list_of_files:
     if filename!="header.html" and filename!="footer.html":
         f=os.path.join(srcFolder,filename)
         d=os.path.join(destFolder,filename)
-        pages+="<li><a href="+filename+">"+filename+"</a></li>"
+        pages+="<li><a href="+filename+">"+filename.replace(".html","")+"</a></li>"
         os.system("cat "+ head + " > " + d)
         os.system("cat "+ f + " >> " + d)
         os.system("cat "+ foot + " >> " + d)
