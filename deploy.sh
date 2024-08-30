b335fa784935c3d2e7f6e4681cdc60ab9e3c36c7
nyk0si <nicksiv@disroot.org>
2024-08-30 13:54:46 +0300

    rebuilding site Fri Aug 30 01:54:46 PM EEST 2024

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd ~/nicksiv.github.io
 rsync -av --delete ~/Documents/site/pub/ ~/nicksiv.github.io/site ;
+#pub only customizations
+echo ".prive{visibility:none;" >> style.css
+
 # If a command fails then the deploy stops
 set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
